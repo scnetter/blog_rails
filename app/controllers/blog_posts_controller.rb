919b8fbd4e3abf78ff32519d678ecc479d5bae4b
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-01-09 21:47:52 -0600

    Added blog_post#show method to display individual posts by id

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,4 +2,10 @@ class BlogPostsController < ApplicationController
   def index
     @blog_posts = BlogPost.all
   end
+
+  def show
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+      redirect_to root_path, notice: "Blog post not found"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # Enables blog_post_path() and blog_post_url() when as: :blog_post is used
+  # post
+  # put
+  # patch
+  # delete
   # Defines the root path route ("/")
   # root "articles#index"
   root "blog_posts#index"
