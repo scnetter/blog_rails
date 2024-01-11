3f52808892d7f561890e7b3fcbd8eeb3e998a115
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-01-10 18:57:56 -0600

    Added controller method and form to create new post with validation and using strong params.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  post "blog_posts", to: "blog_posts#create", as: :blog_posts
   # Enables blog_post_path() and blog_post_url() when as: :blog_post is used
   # post
   # put
@@ -9,4 +11,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "blog_posts#index"
+
 end
