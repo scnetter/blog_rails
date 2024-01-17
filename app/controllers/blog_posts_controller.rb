017b0b7140dac9c7c459f63e45929021cc448523
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-01-16 19:14:36 -0600

    Added destroy method for posts and refactored to use resources to generate routes and a helper to find posts by id.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,12 +1,12 @@
 class BlogPostsController < ApplicationController
+
+  before_action :set_blog_post, except: [:index, :new, :create]
+
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-      redirect_to root_path, notice: "Blog post not found"
   end
 
   def new
@@ -23,11 +23,9 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
   end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
@@ -35,9 +33,21 @@ class BlogPostsController < ApplicationController
     end
   end
 
+  def destroy
+    @blog_post.destroy
+    redirect_to root_path, notice: "Blog post deleted"
+  end
+
+
   private
 
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id]) if params[:id]
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, notice: "Blog post not found"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "/blog_posts/edit/:id", to: "blog_posts#edit", as: :edit_blog_post
-  get "blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  post "blog_posts", to: "blog_posts#create", as: :blog_posts
-  patch"blog_posts/:id", to: "blog_posts#update"
+
+  resources :blog_posts
+  # get "blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/edit/:id", to: "blog_posts#edit", as: :edit_blog_post
+  # get "blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # post "blog_posts", to: "blog_posts#create", as: :blog_posts
+  # patch "blog_posts/:id", to: "blog_posts#update"
+  # delete "blog_posts/:id", to: "blog_posts#destroy"
   # Enables blog_post_path() and blog_post_url() when as: :blog_post is used
   # post
   # put
